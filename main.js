415d883ad8a4fe52d67f6cb3ceed0e3b80ad432e
betty_peng <betty_peng@brown.edu>
2015-12-17 12:51:54 -0500

    v2: video, About, and Projects

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 $(document).ready(function() { 
-    $('.carousel').carousel({
+    // $('.carousel').carousel({
+    // });
+
+    $(function($){
+        var addToAll = false;
+        var gallery = false;
+        var titlePosition = 'inside';
+        $(addToAll ? 'img' : 'img.fancybox').each(function(){
+            var $this = $(this);
+            var title = $this.attr('title');
+            var src = $this.attr('data-big') || $this.attr('src');
+            var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+            $this.wrap(a);
+        });
+        if (gallery)
+            $('a.fancybox').attr('rel', 'fancyboxgallery');
+        $('a.fancybox').fancybox({
+            titlePosition: titlePosition
+        });
     });
+    $.noConflict();
+
+
+
+
+
 });
