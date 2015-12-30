b7fe36c7c0497119e80f6f210c96aa3b2eda5fa4
betty_peng <betty_peng@brown.edu>
2015-12-30 03:33:31 -0500

    Added favicon, banner, and improved Contact page.

--- fancybox.js
+++ fancybox.js
@@ -22,24 +22,10 @@ $(document).ready(function() {
     $("#spring15").hide();
 
     $(".season").click(function(e){ 
-        // console.log("HI");
         e.preventDefault();
-        var url = $(this).attr('href'); //get the link you want to load data from
-        // $.ajax({ 
-        //  type: 'GET',
-        //  url: url,
-        //  success: function(data) { 
-        if(url ==="spring15"){
-            $("#fall15").fadeOut(1000);
-            $("#spring15").fadeIn(2000);
-        }
-        else if(url ==="fall15"){
-            $("#spring15").fadeOut(1000);
-            $("#fall15").fadeIn(2000);
-        }
-            // $("#projects").load(url); 
-     //    } 
-     //   }); 
+        var url =  $(this).attr('href'); 
+        $("#projects>div").not(document.getElementById(url)).fadeOut(1000);
+        $("#" + url).fadeIn(2000);
      });(jQuery);
 
 });
