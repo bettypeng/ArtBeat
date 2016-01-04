051eb5cf413b508ca5e207e8854666322e3b3681
betty_peng <betty_peng@brown.edu>
2016-01-04 05:44:02 -0500

    Panels created and Artists of Brown added

--- fancybox.js
+++ fancybox.js
@@ -28,4 +28,11 @@ $(document).ready(function() {
         $("#" + url).fadeIn(2000);
      });(jQuery);
 
+    $('.slide').click(function(){
+        $('html, body').animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 500);
+        return false;
+    });(jQuery);
+
 });
